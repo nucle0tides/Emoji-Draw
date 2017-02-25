c8127f724791d9a879172d56671909af735e8c73
Gabby Ortman <ortmangl@gmail.com>
2017-02-25 16:02:15 -0600

    ADD EMOJIS TO THE CANVAS

--- static/js/draw.js
+++ static/js/draw.js
@@ -3,6 +3,28 @@ var canvas = new fabric.Canvas('drawing-canvas');
 var img_loader = document.getElementById('img_load');
 img_loader.addEventListener('change', handle_img, false);
 
+$('.emoji').click(function(){
+    console.log($(this).attr('id')); 
+
+    for (var i = 0, len = 15; i < len; i++) {
+        fabric.Image.fromURL('static/images/emojis/' + $(this).attr('id') + '.png', function(img) {
+          img.set({
+            left: fabric.util.getRandomInt(0, 600),
+            top: fabric.util.getRandomInt(0, 500),
+            angle: fabric.util.getRandomInt(0, 90)
+          });
+
+          img.perPixelTargetFind = true;
+          img.targetFindTolerance = 4;
+          img.hasControls = img.hasBorders = false;
+
+          img.scale(fabric.util.getRandomInt(50, 100) / 100);
+
+          canvas.add(img);
+        });
+    }  
+});
+
 
 function handle_img(e){
     var reader = new FileReader();
