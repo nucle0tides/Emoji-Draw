1b2450926df75d7000e4775f1f2c99c6a68e4345
Gabby Ortman <ortmangl@gmail.com>
2017-02-25 13:22:36 -0600

    meh

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-if __name__ == "__main__":
-    app.run()
+if __name__ == '__main__':
+	app.debug = True
+	app.run(port = 5000)
