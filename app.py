cba6001071609e9ce457044ff88c08c708166e6c
Gabby Ortman <ortmangl@gmail.com>
2017-02-25 13:18:18 -0600

    scrape emojis

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from flask import Flask, render_template, request, redirect, url_for
+import sqlite3
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
 if __name__ == '__main__':
 	app.debug = True
 	app.run(port = 8000)
+
+	
