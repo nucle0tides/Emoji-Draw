15f30b662c3c418f9ee66fb758b3de7e6f0afdb1
Ashley Dvorsky <advorsky@iastate.edu>
2017-02-25 22:03:50 -0600

    found out that return has two r's in it

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, jsonify, json
 import sqlite3
 app = Flask(__name__)
 
@@ -12,25 +12,34 @@ def index():
 @app.route('/getEmojiByID/<id>', methods=['GET'])
 def get_emoji_by_id(id):
 	db = connect_db() 
-	curr = db.execute("SELECT emoji_name FROM categories WHERE id=?", id)
+	curr = db.cursor()
+	curr = db.execute("SELECT emoji_name FROM categories WHERE id=(?)", (id,))
 	curr = curr.fetchall()
-	return jsonify(curr[0])
-	return curr[0]
+	return jsonify(curr)
+	# return curr[0]
 
 
-@app.route('/getEmojiByID/<name>', methods=['GET'])
+@app.route('/getEmojiByName/<name>', methods=['GET'])
 def get_emoji_by_name(name):
 	db = connect_db() 
-	curr = db.execute("SELECT emoji_name FROM categories WHERE emoji_name=?", name)
-	curr = curr.fetchall()
-	return jsonify(curr[0])
+	curr = db.cursor()
+	curr = curr.execute("SELECT emoji_name FROM categories WHERE emoji_name=(?)", (name,))
+	curr = curr.fetchone()
+	return jsonify(curr)
+
 
 
 @app.route('/getEmojiByCategory/<category>', methods=['GET'])
 def get_emoji_by_category(category):
 	db = connect_db() 
-	curr = db.execute("SELECT * FROM categories WHERE category=(?)", (category,))
-	return jsonify(curr[0][1])
+	curr = db.cursor()
+	curr = db.execute("SELECT emoji_name FROM categories WHERE category=(?)", (category,))
+	emoji_list = []
+	for emoji in curr:
+		emoji_list.append(emoji[0])
+		print type(emoji[0])
+	return json.dumps(emoji_list)
+
 
 
 if __name__ == '__main__':
