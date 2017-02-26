45cf1807fbb843e1f58fe13fbef336b5b78d7eb4
Ashley Dvorsky <advorsky@iastate.edu>
2017-02-26 02:06:34 -0600

    added better search capabilities

--- app.py
+++ app.py
@@ -61,16 +61,66 @@ def get_emoji(term):
 	curr = curr.fetchall()
 	return jsonify(curr)
 
-# @app.route('/getEmojiContains/<term>', methods=['GET'])
-# def get_emoji_contains(term):
-# 	db = connect_db() 
-# 	curr = db.cursor()
-# 	#curr = curr.execute("SELECT emoji_name FROM categories WHERE emoji_name LIKE '%face%'")
-# 	curr = curr.execute("SELECT emoji_name FROM categories WHERE emoji_name LIKE '".$?."%'",(term,))
-# 	curr = curr.fetchall()
-
-# 	return jsonify(curr)
-
+@app.route('/getEmojiContains/<term>', methods=['GET'])
+def get_emoji_contains(term):
+	print(term)
+	thing = term
+	print(thing)
+	db = connect_db() 
+	curr = db.cursor()
+	if term == 'face':
+		curr = curr.execute("SELECT emoji_name FROM categories WHERE emoji_name LIKE '%face%'")
+	elif term == 'medal':
+		curr = curr.execute("SELECT emoji_name FROM categories WHERE emoji_name LIKE '%medal%'")
+	elif term == 'sign':
+		curr = curr.execute("SELECT emoji_name FROM categories WHERE emoji_name LIKE '%sign%'")	
+	elif term == 'baby':
+		curr = curr.execute("SELECT emoji_name FROM categories WHERE emoji_name LIKE '%baby%'")		
+	elif term == 'arrow':
+		curr = curr.execute("SELECT emoji_name FROM categories WHERE emoji_name LIKE '%arrow%'")
+	elif term == 'hand':
+		curr = curr.execute("SELECT emoji_name FROM categories WHERE emoji_name LIKE '%hand%'")	
+	elif term == 'black':
+		curr = curr.execute("SELECT emoji_name FROM categories WHERE emoji_name LIKE '%black'")
+	elif term == 'white':
+		curr = curr.execute("SELECT emoji_name FROM categories WHERE emoji_name LIKE '%white%'")	
+	elif term == 'blonde':
+		curr = curr.execute("SELECT emoji_name FROM categories WHERE emoji_name LIKE '%blonde'")
+	elif term == 'hair':
+		curr = curr.execute("SELECT emoji_name FROM categories WHERE emoji_name LIKE '%hair%'") 	
+	elif term == 'fish':
+		curr = curr.execute("SELECT emoji_name FROM categories WHERE emoji_name LIKE '%fish%'") 	
+	elif term == 'boy':
+		curr = curr.execute("SELECT emoji_name FROM categories WHERE emoji_name LIKE '%boy%'")	
+	elif term == 'girl':
+		curr = curr.execute("SELECT emoji_name FROM categories WHERE emoji_name LIKE '%girl'") 	
+	elif term == 'dark':
+		curr = curr.execute("SELECT emoji_name FROM categories WHERE emoji_name LIKE '%dark%'") 	
+	elif term == 'light':
+		curr = curr.execute("SELECT emoji_name FROM categories WHERE emoji_name LIKE '%light%'") 	
+	elif term == 'medium':
+		curr = curr.execute("SELECT emoji_name FROM categories WHERE emoji_name LIKE '%medium%'")
+	elif term == 'cat':
+		curr = curr.execute("SELECT emoji_name FROM categories WHERE emoji_name LIKE '%cat%'") 
+	elif term == 'heart':
+		curr = curr.execute("SELECT emoji_name FROM categories WHERE emoji_name LIKE '%heart%'")
+	elif term == 'kiss':
+		curr = curr.execute("SELECT emoji_name FROM categories WHERE emoji_name LIKE '%kiss%'")
+	elif term == 'button':
+		curr = curr.execute("SELECT emoji_name FROM categories WHERE emoji_name LIKE '%button%'") 	
+	elif term == 'man':
+		curr = curr.execute("SELECT emoji_name FROM categories WHERE emoji_name LIKE '%man%'")
+	elif term == 'woman':
+		curr = curr.execute("SELECT emoji_name FROM categories WHERE emoji_name LIKE '%woman%'")
+	elif term == 'person':
+		curr = curr.execute("SELECT emoji_name FROM categories WHERE emoji_name LIKE '%person%'")
+	elif term == ('smile') :
+		curr = curr.execute("SELECT emoji_name FROM categories WHERE emoji_name LIKE '%smile' OR emoji_name LIKE '%smiling'")
+	#curr = curr.execute("SELECT emoji_name FROM categories WHERE emoji_name LIKE '%face%'")
+	#curr = curr.execute("SELECT emoji_name FROM categories WHERE emoji_name LIKE ('%' + thing + '%')")
+	curr = curr.fetchall()
+	return jsonify(curr)
+# List<Account> accts = [SELECT Id, Name FROM Account WHERE name LIKE :('%' + searchName + '%')];
 
 if __name__ == '__main__':
 	app.debug = True
